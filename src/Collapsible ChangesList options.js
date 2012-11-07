8ffc1d4d0a89f7706d54227bc240cfa919cd559b
Helder <he7d3r@gmail.com>
2012-11-07 03:01:05 +0000

    Add tracking link and a closure, and validate with JSLint

--- src/Collapsible ChangesList options.js
+++ src/Collapsible ChangesList options.js
@@ -1,16 +1,21 @@
 /**
  * Make recent changes and watchlist options collapsible
- *
  * @source: [[mw:Snippets/Collapsible ChangesList options]]
  * @rev: 2
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Collapsible ChangesList options.js]] ([[File:User:Helder.wiki/Tools/Collapsible ChangesList options.js]])
  */
-if ( $.inArray( mw.config.get( 'wgCanonicalSpecialPageName' ), ['Watchlist', 'Recentchanges']) != -1 ) {
-	var 	$options = $( '#mw-watchlist-options, .rcoptions' ),
+/*jslint browser: true, white: true*/
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
+if ( $.inArray( mw.config.get( 'wgCanonicalSpecialPageName' ), ['Watchlist', 'Recentchanges']) !== -1 ) {
+	var $options = $( '#mw-watchlist-options, .rcoptions' ),
 		$legend = $options.find( 'legend' );
 	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Watchlist' ) {
 		$options.contents().filter( function() {
 				// Keep text-nodes, filter out the rest
-				return this.nodeType == 3;
+				return this.nodeType === 3;
 			} )
 			// Wrap the second text node (intro paragraph) in a tag so we can easily exempt it later when toggling the options
 			.eq(1).wrap( '<p id="mw-watchlist-options-intro" />' );
@@ -28,3 +33,5 @@ if ( $.inArray( mw.config.get( 'wgCanonicalSpecialPageName' ), ['Watchlist', 'Re
 		.prependTo( $options ); // Put the legend and intro outside the wrapper
 	$( '#mw-options-wrapper' ).hide( ); // Hide by default
 }
+
+}( mediaWiki, jQuery ) );
